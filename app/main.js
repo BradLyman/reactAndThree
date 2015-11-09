e9c8ff901a3fdf0920a6d82517dd7501a85921d0
brad <lyman.brad3211@gmail.com>
2015-11-09 16:43:05 -0500

    updated for new react and router

--- app/main.js
+++ app/main.js
@@ -1,22 +1,61 @@
 (function() {
-  var React  = require('react'),
+  'use strict';
+  console.log('loaded');
+  let Three       = require('three'),
+      React       = require('react'),
+      ReactDOM    = require('react-dom'),
       ReactRouter = require('react-router'),
-      Router = ReactRouter.Router,
-      Route  = ReactRouter.Route;
+      Router      = ReactRouter.Router,
+      Route       = ReactRouter.Route,
+      Link        = ReactRouter.Link;
 
-  var injectTapEventPlugin = require('react-tap-event-plugin');
-  var AppHeader = require('./AppHeader.jsx'),
-      BadLocation = require('./BadLocation.jsx'),
-      Scene       = require('./Scene.jsx');
+  const App = React.createClass({
+    render : function() {
+      return (
+        <div>
+          <p>App</p>
+          <Link to="/Scene">Scene</Link>
+          <span> | </span>
+          <Link to="/About">About</Link>
+          <hr />
+          {this.props.children}
+        </div>
+      );
+    },
+  });
 
-  injectTapEventPlugin();
+  const About = React.createClass({
+    render : function() {
+      return <h3>About</h3>;
+    },
+  });
 
-  var routes = (
-    <Route component={AppHeader} >
-      <Route path="/" component={Scene} />
-      <Route path="*" component={BadLocation} />
+  const Scene = React.createClass({
+    componentDidMount : function() {
+      this.scene = new Three.Scene();
+      this.camera = new Three.PerspectiveCamera(
+          75, 800/600, 1, 400);
+      this.renderer = new Three.WebGLRenderer();
+    },
+
+    render : function() {
+      return (
+        <div style={{ border : '1px solid black' }}>
+          <p>Scene</p>
+          <canvas
+            style={{ width : '100%', height : '100%' }}
+            ref="canvas" />
+        </div>
+      );
+    },
+  });
+
+  const Routes = (
+    <Route path="/" component={App}>
+      <Route path="/About" component={About} />
+      <Route path="/Scene" component={Scene} />
     </Route>
   );
 
-  React.render(<Router>{routes}</Router>, document.body);
+  ReactDOM.render(<Router>{Routes}</Router>, document.body);
 }());
